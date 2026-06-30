bd9cf2edc2608344e50ba3c502669e5155b04f87
MallaSahasri <navyamalla2010@gmail.com>
2026-06-30 15:34:18 +0530

    Refactor InventoryManagementSystem to Main class

--- Inventory Management System.java
+++ Inventory Management System.java
@@ -13,14 +13,15 @@ class Product {
         return productId + " " + productName + " Qty:" + quantity + " Price:" + price;
     }
 }
-public class InventoryManagementSystem {
+public class Main {
     public static void main(String[] args) {
         HashMap<Integer, Product> inventory = new HashMap<>();
         inventory.put(1, new Product(1, "Laptop", 10, 50000));
         inventory.put(2, new Product(2, "Mouse", 20, 500));
         inventory.get(1).quantity = 15;
         inventory.remove(2);
-        for(Product p : inventory.values())
+        for (Product p : inventory.values()) {
             System.out.println(p);
+        }
     }
 }
